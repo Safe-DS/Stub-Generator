8e01d2d91f1366676e963d00dfd465c427f55166
Arsam <arsamislami@yahoo.de>
2024-02-29 13:44:50 +0100

    Added more snapshot tests for type var and adjusted the variance tests

--- tests/data/various_modules_package/variance_module.py
+++ tests/data/various_modules_package/variance_module.py
@@ -5,14 +5,27 @@ class A:
     ...
 
 
-_T_co = TypeVar("_T_co", covariant=True, bound=str)
-_T_con = TypeVar("_T_con", contravariant=True, bound=A)
-_T_in = TypeVar("_T_in", int, Literal[1, 2])
+_T_in = TypeVar("_T_in")
+_T_co = TypeVar("_T_co", covariant=True)
+_T_con = TypeVar("_T_con", contravariant=True)
 
 
-class VarianceClassAll(Generic[_T_co, _T_con, _T_in]):
+class VarianceClassOnlyCovarianceNoBound(Generic[_T_co]):
     ...
 
 
-class VarianceClassOnlyInvariance(Generic[_T_in]):
+class VarianceClassOnlyVarianceNoBound(Generic[_T_in]):
+    ...
+
+
+class VarianceClassOnlyContravarianceNoBound(Generic[_T_con]):
+    ...
+
+
+_T_co2 = TypeVar("_T_co2", covariant=True, bound=str)
+_T_con2 = TypeVar("_T_con2", contravariant=True, bound=A)
+_T_in2 = TypeVar("_T_in2", int, Literal[1, 2])
+
+
+class VarianceClassAll(Generic[_T_co2, _T_con2, _T_in2]):
     ...
