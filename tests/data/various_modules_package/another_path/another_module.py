79000ac85885874e3c1bd1c959ee740d2d76dba0
Arsam <arsamislami@yahoo.de>
2024-05-21 17:39:09 +0200

    fixing a bug where internal superclasses are created for stubs despite being internal + Inner classes are also added from internal superclasses

--- tests/data/various_modules_package/another_path/another_module.py
+++ tests/data/various_modules_package/another_path/another_module.py
@@ -2,7 +2,23 @@
 
 Full Docstring Description
 """
+from abc import ABC, abstractmethod
 
 
 class AnotherClass:
     pass
+
+
+class _AnotherPrivateClass(ABC):
+    class AnotherNestedClass:
+        ...
+
+    def another_method(self): ...
+
+
+class _YetAnotherPrivateClass(ABC):
+    class YetAnotherNestedClass(ABC):
+        ...
+
+    @abstractmethod
+    def yet_another_method(self): ...
