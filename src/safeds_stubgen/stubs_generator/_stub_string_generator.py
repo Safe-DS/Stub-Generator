d75983abd483ff46b232fee862ea2d99df6bdf36
Arsam Islami <arsamislami@yahoo.de>
2024-06-20 09:50:47 +0200

    fix: Nested internal classes won't create stubs (#154)
    Closes #143 
    
    ### Summary of Changes
    Fixed a bug where nested internal classes would create stubs even though
    stubs should not be created for these internal classes.

--- src/safeds_stubgen/stubs_generator/_stub_string_generator.py
+++ src/safeds_stubgen/stubs_generator/_stub_string_generator.py
@@ -262,12 +262,15 @@ class StubsStringGenerator:
 
         # Inner classes
         for inner_class in class_.classes:
-            class_string = self._create_class_string(
-                class_=inner_class,
-                class_indentation=inner_indentations,
-                in_reexport_module=in_reexport_module,
-            )
-            class_text += f"\n{class_string}\n"
+            if inner_class.is_public:
+                # We set in_reexport_module to True since nested classes alone can't be reexported and are bound to
+                # their parent class
+                class_string = self._create_class_string(
+                    class_=inner_class,
+                    class_indentation=inner_indentations,
+                    in_reexport_module=True,
+                )
+                class_text += f"\n{class_string}\n"
 
         # Methods
         class_method_text, added_class_methods = self._create_class_method_string(class_.methods, inner_indentations)
