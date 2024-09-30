ee4c77b143ae2ac299305b2b2bbe68d3a7144f04
Patrick Müller <patrick.1999@web.de>
2024-09-30 23:30:24 +0200

    feat: added boundaries and valid_values
    boundaries and valid values are added to ParameterDocstring, also updated the corresponding tests

--- src/safeds_stubgen/docstring_parsing/_docstring.py
+++ src/safeds_stubgen/docstring_parsing/_docstring.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import dataclasses
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
+from safeds_stubgen.api_analyzer._extract_boundary_values import extract_boundary
+from safeds_stubgen.api_analyzer._extract_valid_values import extract_valid_literals
+from safeds_stubgen.api_analyzer._types import BoundaryType
 
 if TYPE_CHECKING:
     from typing import Any
@@ -30,15 +33,35 @@ class FunctionDocstring:
         return dataclasses.asdict(self)
 
 
-@dataclass(frozen=True)
+@dataclass(unsafe_hash=True)  # unsafe hash so that we wont have to call the extractors before init
 class ParameterDocstring:
     type: AbstractType | None = None
     default_value: str = ""
     type_string: str = ""
     description: str = ""
+    boundaries: frozenset[BoundaryType] | None = None
+    valid_values: frozenset[str] | None = None
 
-    def to_dict(self) -> dict[str, Any]:
-        return dataclasses.asdict(self)
+    def __init__(self, type: AbstractType | None = None, default_value: str = "", type_string: str = "", description: str = ""):
+        self.type = type # from annotation
+        self.default_value = default_value
+        self.type_string = type_string
+        self.description = description
+        self.boundaries = frozenset(extract_boundary(description, type_string))
+        self.valid_values = frozenset(extract_valid_literals(description, type_string))
+
+    def to_dict(self) -> dict[str, Any]:  # custom to dict function, as sets are not JSON serializable
+        boundaries = list(self.boundaries) if self.boundaries is not None else []
+        valid_values = sorted(self.valid_values) if self.valid_values is not None else []
+        type = self.type.to_dict() if self.type is not None else {}
+        return {
+            "type": type,
+            "default_value": self.default_value,
+            "type_string": self.type_string,
+            "description": self.description,
+            "boundaries": sorted(map(lambda boundary: boundary.to_dict(), boundaries), key=(lambda boundary_dict: boundary_dict["min"])),
+            "valid_values": valid_values
+        }
 
 
 @dataclass(frozen=True)
--- tests/data/boundary_enum_package_googledoc/main_module_googledoc.py
+++ tests/data/boundary_enum_package_googledoc/main_module_googledoc.py
@@ -1,15 +1,18 @@
 """GoogleDoc Docstring of the some_class.py module."""
 
 # noinspection PyUnusedLocal
-def global_func1_google(param1, param2):
+def global_func1_google(param1, param2, param3):
     """Lorem ipsum
 
     Args:
-    param1 (str):   'If "mean", then replace missing values using the mean along each column. '
-                    'If "median", then replace missing values using the median along each column. '
-                    'If "most_frequent", then replace missing using the most frequent value along each column. '
-                    'If "constant", then replace missing values with fill_value.'
+        param1 (str): If "mean", then replace missing values using the mean along each column. 
+            If "median", then replace missing values using the median along each column. 
+            If "most_frequent", then replace missing using the most frequent value along each column. 
+            If "constant", then replace missing values with fill_value.
+        param2 (str or bool): Valid values are [False, None, 'sparse matrix']
+        param3 (float): Damping factor in the range [0.5, 1.0) is the extent to which the current value is maintained relative
+            to incoming values (weighted 1 - damping). This in order to avoid numerical oscillations when
+            updating these values (messages).
 
-    param2 (str or bool): Valid values are [False, None, 'sparse matrix']
     """
     pass
